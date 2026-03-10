074a7b4f7094e7f4e211b3bcba2ef19f35888dc3
jitendraverma2007 <2007jitendraverma@gmail.com>
2026-03-10 15:31:52 +0530

    Add project files

---
+++ src/main/java/com/chatroom/util/DBUtil.java
@@ -0,0 +1,21 @@
+package com.chatroom.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBUtil {
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/secure_chat_db"
+            + "?useSSL=false"
+            + "&allowPublicKeyRetrieval=true"
+            + "&serverTimezone=UTC";
+
+    private static final String USER = "root";
+    private static final String PASS = "jitendra";
+
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
